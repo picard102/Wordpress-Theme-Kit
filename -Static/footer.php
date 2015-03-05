1e2d9379b46f1e7bb2116921dd404da9b1c269fc
John Leschinski <john.leschinski@exsite.ca>
2015-03-05 12:04:18 -0500

    it's a Start

---
+++ -Static/footer.php
@@ -0,0 +1,16 @@
+<footer id="site_footer">
+<div class="wrapper">
+		
+	<a href="http://www.exsite.ca" id="credit">Site by Exsite Inc <svg><use xlink:href="#logo_exsite"></use></svg></a>
+	
+</div>
+</footer>
+
+<a href="index.php">Home</a> | 
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/main.js"></script>
+<script src="http://10.199.1.198:25711/livereload.js?snipver=1"></script>
+
+</body>
+</html>
